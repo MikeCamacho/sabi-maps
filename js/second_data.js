43f953af9fdd0a4d9a79ec87b76b0601ab4c2fb8
fabiae <yei.claros@udla.edu.co>
2020-08-24 02:00:21 -0500

    feat(app): second mapa

---
+++ js/second_data.js
@@ -0,0 +1,117 @@
+const zones = {
+  east: {
+    origin: { lat: 4.6273523, lng: -74.083301 },
+    destination: { lat: 5.3328799, lng: -72.4261063 },
+    points: {
+      cities: [
+        { lat: 4.3277051, lng: -73.8710759 },//quetame
+        { lat: 4.1201653, lng: -73.643678 },//Villavicencio
+        { lat: 4.0560889, lng: -73.1755426 },//Pachaquiaro
+        { lat: 4.0903233, lng: -72.959896 },//pto lopez
+        { lat: 4.2571163, lng: -73.564102 },//restrepo
+        { lat: 4.2697853, lng: -73.488339 }, //cumaral
+        { lat: 4.3763043, lng: -73.213404 }, //paratebueno
+        { lat: 4.8816743, lng: -72.891351 },//monterrey
+        { lat: 5.0114703, lng: -72.750982 }//tauramena
+      ],
+      type1: [
+        { lat: 5.16869, lng: -72.54503 },
+        { lat: 4.20383, lng: -73.71635 }
+      ],
+      type2: [
+        { lat: 4.619142, lng: -72.925438 },
+        { lat: 4.4419322, lng: -74.0488241 },
+        { lat: 4.31483, lng: -72.09992 }
+      ],
+      matpel: []
+    }
+  },
+  center: {
+    origin: { lat: 4.6273523, lng: -74.083301 },
+    destination: { lat: 7.7610842, lng: -73.4023927 },
+    points: {
+      cities: [
+        { lat: 7.3923933, lng: -73.494368 },//Sabana de torres
+        { lat: 6.5208613, lng: -74.104072 },//Puerto araujo
+        { lat: 6.2608893, lng: -74.489076 },//Zambito
+        { lat: 5.4654002, lng: -74.6591638 },//Puerto salgar
+        { lat: 5.2072293, lng: -74.738393 }, //Honda
+        { lat: 5.4143753, lng: -74.69078 },//El purnio
+        { lat: 5.1897721, lng: -74.7147489 }, //guaduas
+        { lat: 4.9972111, lng: -74.3411389 },//La vega
+        { lat: 4.8071223, lng: -74.355662 },//Facatativa
+        { lat: 4.7329912, lng: -74.2651858 },//Madrid
+      ],
+      type1: [
+        { lat: 5.07723, lng: -74.60063 }
+      ],
+      type2: [
+        { lat: 7.12140, lng: -73.59613 },
+        { lat: 5.93852, lng: -74.57107 },
+        { lat: 4.877034, lng: -74.438353 }
+      ],
+      matpel: [
+        { lat: 4.7064627, lng: -74.2432617 }
+      ]
+    }
+  },
+  nort: {
+    origin: { lat: 7.7610842, lng: -73.4023927 },
+    destination: { lat: 10.9946023, lng: -74.808128 },
+    points: {
+      cities: [
+        { lat: 8.6905393, lng: -73.666125 },//Pelaya
+        { lat: 9.2020793, lng: -73.542328 },//Curumani
+        { lat: 9.7399083, lng: -73.711995 }, //Cuatro vientos
+        { lat: 10.1486463, lng: -73.954705 }, //El Copey
+        { lat: 10.5916773, lng: -74.171903 }, //Aracataca
+        { lat: 11.1805383, lng: -74.229934 },//Gaira
+        { lat: 10.9933923, lng: -74.946651 },//Puerto Colombia
+        { lat: 10.8762448, lng: -75.0827786 },//Santa veronica
+        { lat: 10.7338193, lng: -75.2648022 },//Loma de arena
+        { lat: 10.3933443, lng: -75.485486 },//Cartagena
+        { lat: 10.6022583, lng: -75.288912 },//Santa catalina
+        { lat: 10.6384052, lng: -74.9266825 },//Sabanalarga
+      ],
+      type1: [
+        { lat: 9.977575, lng: -73.88730 }
+      ],
+      type2: [
+        { lat: 8.310301, lng: -73.622421 },
+        { lat: 11.0091387, lng: -74.2808352 }
+      ],
+      matpel: [
+        { lat: 9.7176884, lng: -75.1408309 }
+      ]
+    }
+  },
+  south: {
+    origin: { lat: 4.6273523, lng: -74.083301 },
+    destination: { lat: 1.6146454, lng: -75.607163 },
+    points: {
+      cities: [
+        { lat: 2.0642375, lng: -75.791772 },//Altamira
+        { lat: 2.5833424, lng: -75.453968 },//Hobo
+        { lat: 2.9423014, lng: -75.313008 },//Neiva
+        { lat: 3.2221164, lng: -75.2498427 },//Aipe
+        { lat: 3.6255504, lng: -75.1019597 },//Natagaima
+        { lat: 4.0262693, lng: -74.969608 },//Guamo
+        { lat: 4.1499521, lng: -74.901375 },//Espinal
+        { lat: 4.3054013, lng: -74.805155 },//Girardot
+        { lat: 4.2054513, lng: -74.647517 },//Melgar
+        { lat: 4.3426943, lng: -74.364582 },//Fusagasuga
+        { lat: 4.5668648, lng: -74.9647717 },//Alvarado
+        { lat: 3.3952332, lng: -76.5958754 },//Cali
+      ],
+      type1: [
+        { lat: 4.4046853, lng: -75.181892 }
+      ],
+      type2: [
+        { lat: 2.3845279, lng: -75.5548267 }
+      ],
+      matpel: [
+        { lat: 3.5320986, lng: -76.3305307 }
+      ]
+    }
+  }
+}
---
+++ js/second_map.js
@@ -0,0 +1,232 @@
+//DROP DOWNS
+const dropDownBtnZones = document.getElementById('dropDownBtnZones')
+const dropDownPanelZones = document.getElementById('dropDownPanelZones')
+dropDownBtnZones.addEventListener('click', function () {
+  dropDownPanelZones.classList.toggle('show')
+})
+
+const zoneEast = document.getElementById('zoneEast')
+const zoneCenter = document.getElementById('zoneCenter')
+const zoneNort = document.getElementById('zoneNort')
+const zoneWest = document.getElementById('zoneWest')
+const zoneSouth = document.getElementById('zoneSouth')
+
+const buttonAllZones = document.getElementById('allZones')
+
+var icon_cities = "https://clientes.mi-martinez.com/sabi/wp-content/uploads/2020/08/icon-point-purple.png"
+var icon_type1 = "https://clientes.mi-martinez.com/sabi/wp-content/uploads/2020/08/icon-1.png"
+var icon_type2 = "https://clientes.mi-martinez.com/sabi/wp-content/uploads/2020/08/icon-2.png"
+var icon_matpel = "https://clientes.mi-martinez.com/sabi/wp-content/uploads/2020/08/icon-3.png"
+
+const titleZone = document.getElementById('titleZone')
+
+function initMap() {
+  var directionsService = new google.maps.DirectionsService()
+  var directionsRenderer = new google.maps.DirectionsRenderer()
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 6.4920373, lng: -75.5535881 },
+    zoomControl: true,
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false,
+  })
+  map.setOptions({ zoom: 6.6, center: { lat: 6.4920373, lng: -75.5535881 } })
+  directionsRenderer.setMap(map)
+
+  var rendererEast = new google.maps.DirectionsRenderer({ preserveViewport: true })
+  let markersEast = { points: [], cities: [], type1: [], type2: [], matpel: [] }
+
+  var rendererCenter = new google.maps.DirectionsRenderer({ preserveViewport: true })
+  let markersCenter = { points: [], cities: [], type1: [], type2: [], matpel: [] }
+
+  var rendererNort = new google.maps.DirectionsRenderer({ preserveViewport: true })
+  let markersNort = { points: [], cities: [], type1: [], type2: [], matpel: [] }
+
+  var rendererSouth = new google.maps.DirectionsRenderer({ preserveViewport: true })
+  let markersSouth = { points: [], cities: [], type1: [], type2: [], matpel: [] }
+
+  function selectZoneEast(state, all) {
+    if (state) {
+      if (!all) {
+        selectZoneCenter(false)
+        selectZoneNort(false)
+        selectZoneSouth(false)
+        titleZone.innerText = 'Zona 1 - Oriente'
+        rendererEast.setOptions({ preserveViewport: false })
+      } else {
+        rendererEast.setOptions({ preserveViewport: true })
+      }
+      selectZone(zoneEast, rendererEast, map, markersEast)
+      dropDownPanelZones.classList.remove('show')
+    } else {
+      deselectZone(zoneEast, rendererEast, map, markersEast)
+    }
+  }
+  zoneEast.addEventListener('click', () => selectZoneEast(true))
+
+  function selectZoneCenter(state, all) {
+    if (state) {
+      if (!all) {
+        selectZoneEast(false)
+        selectZoneNort(false)
+        selectZoneSouth(false)
+        titleZone.innerText = 'Zona 2 - Center'
+        rendererCenter.setOptions({ preserveViewport: false })
+      } else {
+        rendererCenter.setOptions({ preserveViewport: true })
+      }
+      selectZone(zoneCenter, rendererCenter, map, markersCenter)
+      dropDownPanelZones.classList.remove('show')
+    } else {
+      deselectZone(zoneCenter, rendererCenter, map, markersCenter)
+    }
+  }
+  zoneCenter.addEventListener('click', () => selectZoneCenter(true))
+
+  function selectZoneNort(state, all) {
+    if (state) {
+      if (!all) {
+        selectZoneCenter(false)
+        selectZoneEast(false)
+        selectZoneSouth(false)
+        titleZone.innerText = 'Zona 3 - Norte'
+        rendererNort.setOptions({ preserveViewport: false })
+      } else {
+        rendererNort.setOptions({ preserveViewport: true })
+      }
+      selectZone(zoneNort, rendererNort, map, markersNort)
+      dropDownPanelZones.classList.remove('show')
+    } else {
+      deselectZone(zoneNort, rendererNort, map, markersNort)
+    }
+  }
+  zoneNort.addEventListener('click', () => selectZoneNort(true))
+
+  function selectZoneSouth(state, all) {
+    if (state) {
+      if (!all) {
+        selectZoneCenter(false)
+        selectZoneEast(false)
+        selectZoneNort(false)
+        titleZone.innerText = 'Zona 4 - Sur'
+        rendererSouth.setOptions({ preserveViewport: false })
+      } else {
+        rendererSouth.setOptions({ preserveViewport: true })
+      }
+      selectZone(zoneSouth, rendererSouth, map, markersSouth)
+      dropDownPanelZones.classList.remove('show')
+    } else {
+      deselectZone(zoneSouth, rendererSouth, map, markersSouth)
+    }
+  }
+  zoneSouth.addEventListener('click', () => selectZoneSouth(true))
+
+  function selectAllZones() {
+    selectZoneCenter(true, true)
+    selectZoneEast(true, true)
+    selectZoneNort(true, true)
+    selectZoneSouth(true, true)
+    titleZone.innerText = 'TODOS'
+    map.setOptions({ zoom: 6.6, center: { lat: 6.4920373, lng: -75.5535881 } })
+  }
+
+  buttonAllZones.addEventListener('click', () => selectAllZones())
+
+
+  //INIT ZONES
+  renderZone('east', zoneEast, rendererEast, map, markersEast, directionsService)
+  renderZone('center', zoneCenter, rendererCenter, map, markersCenter, directionsService)
+  renderZone('nort', zoneNort, rendererNort, map, markersNort, directionsService)
+  renderZone('south', zoneSouth, rendererSouth, map, markersSouth, directionsService)
+  titleZone.innerText = 'TODAS'
+}
+
+function renderZone(name_zone, zone, render, map, markers, service) {
+  const { origin, destination } = zones[name_zone]
+  var markerStart = new google.maps.Marker({
+    position: origin,
+    map,
+    icon: icon_cities
+  })
+  var markerEnd = new google.maps.Marker({
+    position: destination,
+    map,
+    icon: icon_cities
+  })
+  markers['points'].push(markerStart, markerEnd)
+  var waypoints = selectedWaypoints(name_zone, markers, map)
+  requestRoute(origin, destination, waypoints, render, service)
+  render.setMap(map)
+}
+
+function requestRoute(origin, destination, waypoints, render, service) {
+  var request = {
+    origin,
+    destination,
+    waypoints,
+    optimizeWaypoints: true,
+    provideRouteAlternatives: true,
+    travelMode: 'DRIVING',
+  }
+  service.route(request, function (response, status) {
+    try {
+      if (status == 'OK') {
+        render.setOptions({ directions: response, suppressPolylines: false, suppressMarkers: true })
+      } else {
+        window.alert('Directions request failed due to ' + status)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  })
+}
+
+function selectedWaypoints(zone, markers, map) {
+  var waypoints = []
+  Object.keys(zones[zone].points).map(key => {
+    const array = zones[zone].points[key]
+    array.forEach(item => {
+      waypoints.push({
+        location: item,
+        stopover: true
+      })
+      let icon = eval('icon_' + key)
+      let marker = new google.maps.Marker({
+        position: item,
+        map,
+        icon,
+      })
+      markers[key].push(marker)
+    })
+  })
+  return waypoints
+}
+
+function selectZone(zone, render, map, markers) {
+  render.setOptions({ suppressPolylines: false, suppressMarkers: true })
+  setMarkersPoints(map, markers)
+  render.setMap(map)
+}
+
+function deselectZone(zone, render, map, markers) {
+  //route.setAttribute('checked', 'false')
+  render.setOptions({ suppressPolylines: true, suppressMarkers: true })
+  setMarkersPoints(null, markers)
+  render.setMap(map)
+}
+
+function setMarkersPoints(map, markers, points) {
+  const object = markers
+  Object.keys(object).map(key => {
+    for (let index = 0; index < object[key].length; index++) {
+      object[key][index].setMap(map)
+    }
+    //object[key] = []
+  })
+}
+
+
+
+
+
+
